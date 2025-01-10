32d4c4b54d573e8cf7d7ed9b7dd3723508d3b19b
Gabriel <gabysolotorevsky100@gmail.com>
2025-01-10 17:07:43 -0300

    Getting into preemptive tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "vga_test/vga.h"
-#include "../modules/mod.cpp"
 #include "libarbys/libarbys.hpp"
+#include "kernel32/scheduler/scheduler.hpp"
+#include "kernel32/interrupts/interrupts.hpp"
 
 #ifndef CYW43_WL_GPIO_LED_PIN
 #define CYW43_WL_GPIO_LED_PIN 0
@@ -21,13 +22,15 @@ int main()
     // start_vga_test();
     string_L buffer;
     buffer.allocate(1024);
+    scheduler_global.create();
     while (true)
     {
-            promise_L prom = read_into(buffer);
+            promise_L prom = buffer.read_character();
             if (prom.is_done)
             {
-                process_command(buffer);
+                buffer.split();
                 printf("finished: %d\n",buffer.size);
+                scheduler_global.execute();
                 buffer.empty();
                 printf("finished2: %d\n",buffer.size);
                 buffer.print();
